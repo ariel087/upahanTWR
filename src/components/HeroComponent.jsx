0b3ac04ebfa5b1cca19ac3808e14c17b0de02088
KhailJuliaMercado <202011380@btech.ph.education>
2024-11-15 23:21:20 +0800

    property updated

--- src/components/HeroComponent.jsx
+++ src/components/HeroComponent.jsx
@@ -211,7 +211,7 @@ const HeroComponent = () => {
 
       <div className="w-full h-10 flex justify-between p-2 whitespace-nowrap">
         <p className="text-xl">Pulilan, Bulacan</p>
-        <div className="flex items-center h-5 w-22">
+        <div className="flex items-center pr-2 space-x-2 h-5 w-22">
           {[...Array(5)].map((_, i) => (
             <svg
               key={i}
@@ -222,8 +222,9 @@ const HeroComponent = () => {
               viewBox="0 0 22 20"
             >
               <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-          ))}
+             </svg>
+            ))} 
+            <span>5.0</span>
         </div>
       </div>
       <p className="text-base italic text-start w-full px-2 text-gray-500">
--- src/components/modals/LoginModalComponent.jsx
+++ src/components/modals/LoginModalComponent.jsx
@@ -34,7 +34,7 @@ const LoginModalComponent = ({ showModal, setShowModal }) => {
                       type="email"
                       id="email"
                       class="bg-gray-50 border  border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      placeholder="name@flowbite.com"
+                      placeholder="name@example.com"
                       required
                     />
                   </div>
